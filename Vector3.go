8dd3d7de17a7ea8e0fbb71ee1ebb429de5f964a3
janis <janis.boersig@gmx.de>
2022-01-10 13:54:58 +0100

    feature: updated Vector3.go

--- Vector3.go
+++ Vector3.go
@@ -8,23 +8,24 @@ type Vector3 struct {
 	Z float32
 }
 
-func (v Vector3) Add(v2 Vector3) Vector3 {
-	v.X += v2.X
-	v.Y += v2.Y
-	v.Z += v2.Z
-
-	return v
+func (v *Vector3) Add(v2 *Vector3) *Vector3 {
+	return &Vector3{v.X + v2.X, v.Y + v2.Y, v.Z + v2.Z}
 }
 
-func (v Vector3) Multiply(num float32) Vector3 {
-	v.X *= num
-	v.Y *= num
-	v.Z *= num
-
-	return v
+func (v *Vector3) Subtract(v2 *Vector3) *Vector3 {
+	return &Vector3{v.X - v2.X, v.Y - v2.Y, v.Z - v2.Z}
 }
 
-func Distance(a, b Vector3) float32 {
+func (v *Vector3) Multiply(num float32) *Vector3 {
+	return &Vector3{v.X * num, v.Y * num, v.Z * num}
+}
+
+func SquaredDistance(a, b *Vector3) float32 {
+	squaredDist := math.Pow(float64(a.X-b.X), 2) + math.Pow(float64(a.Y-b.Y), 2) + math.Pow(float64(a.Z-b.Z), 2)
+	return float32(squaredDist)
+}
+
+func Distance(a, b *Vector3) float32 {
 	dist := math.Sqrt(math.Pow(float64(a.X-b.X), 2) + math.Pow(float64(a.Y-b.Y), 2) + math.Pow(float64(a.Z-b.Z), 2))
 	return float32(dist)
 }
